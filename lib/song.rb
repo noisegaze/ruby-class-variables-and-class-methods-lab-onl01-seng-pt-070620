ad03ead1aa16006f184962dd39ee5e59bb240c4f
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-26 19:28:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,57 @@
+require 'pry'
+class Song 
+  
+  @@count = 0 
+  @@genres =[]
+  @@artists =[]
+  
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name,artist,genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1
+    @@genres << @genre 
+    @@artists << @artist 
+  end
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.genres
+    @@genres.uniq
+  end  
+  
+  def self.artists 
+    @@artists.uniq
+  end  
+  
+  #expected: {"pop"=>1, "rap"=>2}
+  
+  def self.genre_count
+    genre_count = {}
+   @@genres.map do |styles|
+     #binding.pry
+    genre_count[styles]+=1 
+    if [styles] > 1 
+
+   else 
+    
+   end
+    
+     
+      
+  
+  end
+  
+  
+  
+end  
+
+
+
+
+
+
